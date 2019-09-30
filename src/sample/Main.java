c218573430f44cc8bdb384f1c3e4ed34541f0cd5
grib4an <grib.yan98@mail.ru>
2019-09-30 03:43:27 +0300

    create tiktokAPI modules

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,15 @@
 package sample;
 
+import com.eclipsesource.v8.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.File;
 
+public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -17,7 +19,8 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
